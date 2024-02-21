6525530dcd0d9be968bb2bfc4ed0ca6e3150be22
VladBanasko <vlad.bona@gmail.com>
2024-02-21 17:44:32 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>recipeBook 123</h1>
+      <Navbar />
     </>
   )
 }

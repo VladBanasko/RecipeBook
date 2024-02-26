c7a8950fd3cdebacc20ba3b8d59d4571e9695bc2
VladBanasko <vlad.bona@gmail.com>
2024-02-26 18:56:05 -0500

    Update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,13 @@
+import { Link } from "@nextui-org/react"
 import NavbarTile from "../components/NavbarTile"
+
+
 
 const About = () => {
   return (
     <div>
-      <h3 className='text-center '>About</h3>
-      <p className="text-justify mt-2" > Welcome to our Recipe Search SPA (Single Page Application), where culinary inspiration meets convenience! Our intuitive and user-friendly interface allows you to explore a vast world of delectable recipes effortlessly. Powered by a robust <a href="https://rapidapi.com/apininjas/api/recipe-by-api-ninjas" className="underline-offset-4" >Recipes API</a>, our SPA enables you to search through an extensive database to discover a diverse array of culinary delights.
+      <h2 className='text-center '>About</h2>
+      <p className="text-justify mt-2 spa" > Welcome to our Recipe Search SPA (Single Page Application), where culinary inspiration meets convenience! Our intuitive and user-friendly interface allows you to explore a vast world of delectable recipes effortlessly. Powered by a robust <Link isExternal showAnchorIcon href="https://rapidapi.com/apininjas/api/recipe-by-api-ninjas" color="primary">Recipes API</Link> our SPA enables you to search through an extensive database to discover a diverse array of culinary delights.
 
         Simply enter your desired a meal, and let our SPA work its magic. Within seconds, you'll receive a curated list of mouthwatering recipes, complete with detailed instructions and ingredient lists. Whether you're a seasoned chef or a kitchen novice, our Recipe Search SPA is designed to make your cooking experience enjoyable and stress-free.
 
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,23 @@
+import { Link, useRouteError } from 'react-router-dom'
+import img from '../assets/undraw_page_not_found_re_e9o6.svg'
+import { Image } from "@nextui-org/react";
 
 
 const Error = () => {
+  const error = useRouteError()
+  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <div className=' flex min-h-screen flex-col text-center justify-center items-center'>
+        <Image src={img} alt='not found' className=' mb-6 w-5/6 h-5/6 inline-flex ' />
+        <h3 className='mb-2'>Sorry,not found</h3>
+        <Link to='/' className='capitalize'>back home</Link>
+      </div>
+    )
+
+  }
+
   return (
     <div>Error</div>
   )

efc8e7fae8295f98acf82c0a52b308f39d6eff7c
VladBanasko <vlad.bona@gmail.com>
2024-02-29 18:22:01 -0500

    update

--- src/components/NavbarTile.jsx
+++ src/components/NavbarTile.jsx
@@ -1,8 +1,23 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Switch } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Switch, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/react";
 
 
 import Food from '../assets/restaurant.svg?react'
 import Drink from '../assets/cocktail.svg?react'
+
+
+import { ChevronDown, Lock, Activity, Flash, Server, TagUser, Scale } from "../assets/Icons";
+
+const icons = {
+  chevron: <ChevronDown fill="currentColor" size={16} />,
+  scale: <Scale className="text-warning" fill="currentColor" size={30} />,
+  lock: <Lock className="text-success" fill="currentColor" size={30} />,
+  activity: <Activity className="text-secondary" fill="currentColor" size={30} />,
+  flash: <Flash className="text-primary" fill="currentColor" size={30} />,
+  server: <Server className="text-success" fill="currentColor" size={30} />,
+  user: <TagUser className="text-danger" fill="currentColor" size={30} />,
+};
+
+
 
 const NavbarTile = () => {
   return (
@@ -12,16 +27,19 @@ const NavbarTile = () => {
       </NavbarBrand>
 
       {/* Switch to change search from Drinks and Food API */}
-      <Switch
-        defaultSelected
-        size="lg"
-        color="success"
-        startContent={<Food />}
-        endContent={<Drink />}
-      >
-        choose food or drink
-      </Switch>
+      <NavbarContent className="justify-center capitalize">
+        <Switch
+          defaultSelected
+          size="lg"
+          color="success"
+          startContent={<Food />}
+          endContent={<Drink />}
+        >
+          choose food or drink
+        </Switch></NavbarContent>
 
+
+      {/* add categories of drinks  */}
 
       <NavbarContent className="hidden sm:flex gap-4" justify='center'>
         <NavbarItem isActive>
@@ -30,9 +48,64 @@ const NavbarTile = () => {
           </Link>
         </NavbarItem>
         <NavbarItem >
-          <Link href="#" color="foreground">
-            Categories
-          </Link>
+          <Dropdown>
+            <NavbarItem>
+              <DropdownTrigger>
+                <Button
+                  disableRipple
+                  className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+                  endContent={icons.chevron}
+                  radius="sm"
+                  variant="light"
+                >
+                  Categories
+                </Button>
+              </DropdownTrigger>
+            </NavbarItem>
+            <DropdownMenu
+              aria-label="ACME features"
+              className="w-[340px]"
+              itemClasses={{
+                base: "gap-4",
+              }}
+            >
+              <DropdownItem
+                key="Cocktail"
+                description="Cocktail drinks"
+                startContent={icons.scale}
+              >
+                Cocktails
+              </DropdownItem>
+              <DropdownItem
+                key="usage_metrics"
+                description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
+                startContent={icons.activity}
+              >
+                Usage Metrics
+              </DropdownItem>
+              <DropdownItem
+                key="production_ready"
+                description="ACME runs on ACME, join us and others serving requests at web scale."
+                startContent={icons.flash}
+              >
+                Production Ready
+              </DropdownItem>
+              <DropdownItem
+                key="99_uptime"
+                description="Applications stay on the grid with high availability and high uptime guarantees."
+                startContent={icons.server}
+              >
+                +99% Uptime
+              </DropdownItem>
+              <DropdownItem
+                key="supreme_support"
+                description="Overcome any challenge with a supporting team ready to respond."
+                startContent={icons.user}
+              >
+                +Supreme Support
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="/about">
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,12 +2,23 @@ import axios from "axios";
 import { useLoaderData } from "react-router-dom"
 import CocktailList from "../components/CocktailList";
 
+const cocktailCategorySearch = 'www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+
+
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 export const loader = async () => {
   const searchTerm = ''
   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
+  const cat = response.data.drinks.map((item) => {
+    return item.strCategory
+  })
+
+  const categ = new Set(cat)
+  console.log(response.data.drinks);
+  console.log(categ);
+
   return { drinks: response.data.drinks, searchTerm }
 }
 

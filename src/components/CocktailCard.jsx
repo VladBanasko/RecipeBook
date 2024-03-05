525fdeb21bed39d874bc560f52c44b307f4c7b1a
VladBanasko <vlad.bona@gmail.com>
2024-03-05 15:09:17 -0500

    card fixed

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,21 +1,26 @@
 import { Card, CardFooter, Image } from "@nextui-org/react"
-import { Link, useOutletContext } from "react-router-dom"
+import { Link, useOutletContext, useNavigation } from "react-router-dom"
+
+
+
+
 
 const CocktailCard = ({ image, id, name, info, glass }) => {
 
   // const data = useOutletContext()
 
   return (
-    <Card isFooterBlurred
-      radius="lg"
-      className="border-none ">
-      <Image className="object-cover rounded-xl" src={image} alt={name} isZoomed width={240} />
-      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <h4 className="font-bold">{name}</h4>
-        <p>{info}</p>
-        <Link to={`/cocktail/${id}`}>Details</Link>
-      </CardFooter>
-    </Card>
+    <Link to={`/cocktail/${id}`}>
+      <Card isFooterBlurred
+        radius="lg"
+        className="border-none ">
+        <Image className="object-cover rounded-xl" src={image} alt={name} isZoomed width={240} />
+        <CardFooter className="justify-around before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <h4 className="font-bold">{name}</h4>
+          <p>{info}</p>
+        </CardFooter>
+      </Card>
+    </Link>
   )
 }
 export default CocktailCard

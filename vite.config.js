afff56f8a18423bc3174e709d87968c048b0f16c
VladBanasko <vlad.bona@gmail.com>
2024-03-11 15:40:25 -0400

    u

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  build: { chunkSizeWarningLimit: 1600, },
   base: '/',
 })

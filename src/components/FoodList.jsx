aa609801cecd8ea700aa02b6405c28acba02d2bb
VladBanasko <vlad.bona@gmail.com>
2024-03-15 17:38:56 -0400

    update

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -4,7 +4,7 @@ const FoodList = ({ food }) => {
   // implement logic for showing list with meals
   return (
     <>
-      <div>{food}</div>
+      <div className="flex justify-center">{food}</div>
 
     </>
 
